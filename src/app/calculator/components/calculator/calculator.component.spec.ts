6ebca9eb99f0997fd6deb45d5d3ad2e4939d693e
Jhonny <jhonnycgarcia@gmail.com>
2024-10-24 18:31:57 -0500

    Mock CalculatorService

--- src/app/calculator/components/calculator/calculator.component.spec.ts
+++ src/app/calculator/components/calculator/calculator.component.spec.ts
@@ -1,5 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalculatorComponent } from './calculator.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
+
+class MockCalculatorService {
+  public resultText = jasmine.createSpy('resultText')
+    .and.returnValue('100.00');
+  public subResultText = jasmine.createSpy('subResultText')
+    .and.returnValue('20');
+  public lastOperator = jasmine.createSpy('lastOperator')
+    .and.returnValue('-');
+
+  public constructNumber = jasmine.createSpy('constructNumber');
+}
 
 describe('CalculatorComponent', () => {
 
@@ -10,6 +22,9 @@ describe('CalculatorComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CalculatorComponent],
+      providers: [
+        { provide: CalculatorService, useClass: MockCalculatorService }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CalculatorComponent);
@@ -21,6 +36,11 @@ describe('CalculatorComponent', () => {
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have initial values', () => {
+    expect(component.resultText()).toBe('100.00');
+    expect(component.subResultText()).toBe('20');
+    expect(component.lastOperator()).toBe('-');
   });
 
 });
