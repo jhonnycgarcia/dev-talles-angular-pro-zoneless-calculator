94811be60aff3e86e50e9281b285901aa55b5843
Jhonny <jhonnycgarcia@gmail.com>
2024-10-23 23:02:28 -0500

    Host Element - Content Projection

--- src/app/calculator/views/calculator-view/calculator-view.component.ts
+++ src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -1,10 +1,16 @@
-import { CalculatorComponent } from '@/calculator/components/calculator/calculator.component';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { CalculatorButtonComponent } from '@/calculator/components/calculator-button/calculator-button.component';
+import { CalculatorComponent } from '@/calculator/components/calculator/calculator.component';
+
 
 @Component({
   selector: 'calculator-view',
   standalone: true,
-  imports: [ CalculatorComponent ],
+  imports: [
+    CalculatorButtonComponent,
+    CalculatorComponent,
+  ],
   templateUrl: './calculator-view.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
