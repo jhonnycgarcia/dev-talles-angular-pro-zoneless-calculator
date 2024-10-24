1a66d4fff2cfebe572882ee829e764ba2bbb196e
Jhonny <jhonnycgarcia@gmail.com>
2024-10-24 12:14:49 -0500

    Mi primera prueba

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,11 +15,27 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should have the 'zonesless-calculator' title`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('zonesless-calculator');
-  // });
+  it('should be 3', () => {
+
+    // A = Arrange
+    const num1 = 1;
+    const num2 = 2;
+
+    // A = Act
+    const result = num1 + num2;
+
+    // A = Assert
+    // if(result !== 3) {
+    //   throw new Error('Expected 3 but got ' + result);
+    // }
+    expect(result).toBe(3);
+  })
+
+  it(`should have the 'zonesless-calculator' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('zonesless-calculator');
+  });
 
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
@@ -27,4 +43,5 @@ describe('AppComponent', () => {
   //   const compiled = fixture.nativeElement as HTMLElement;
   //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zonesless-calculator');
   // });
+
 });
