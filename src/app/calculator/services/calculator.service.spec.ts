0dfb2a61b6bf33132aa43b981ddfa2cc9a03fec9
Jhonny <jhonnycgarcia@gmail.com>
2024-10-24 12:48:13 -0500

    Pruebas en servicios con señales

--- src/app/calculator/services/calculator.service.spec.ts
+++ src/app/calculator/services/calculator.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
@@ -12,5 +11,11 @@ describe('CalculatorService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be created with default values', () => {
+    expect(service.resultText()).toBe('0');
+    expect(service.subResultText()).toBe('0');
+    expect(service.lastOperator()).toBe('+');
   });
 });
