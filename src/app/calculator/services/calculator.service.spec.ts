63a74c54597d27a0604462692cb3d088d000f666
Jhonny <jhonnycgarcia@gmail.com>
2024-10-24 17:29:25 -0500

    Contruir números y cálculos

--- src/app/calculator/services/calculator.service.spec.ts
+++ src/app/calculator/services/calculator.service.spec.ts
@@ -42,4 +42,80 @@ describe('CalculatorService', () => {
     expect(service.lastOperator()).toBe('+');
   });
 
+  it('should update resultText with number input', () => {
+    service.constructNumber('1');
+    expect(service.resultText()).toBe('1');
+
+    service.constructNumber('2');
+    expect(service.resultText()).toBe('12');
+
+    service.constructNumber('3');
+    expect(service.resultText()).toBe('123');
+  });
+
+  it('should handler operators correctly', () => {
+    service.constructNumber('1');
+    service.constructNumber('-');
+
+    expect(service.lastOperator()).toBe('-');
+    expect(service.subResultText()).toBe('1');
+    expect(service.resultText()).toBe('0');
+  });
+
+
+  it('should calculate result correctly for addition "+"', () => {
+    service.constructNumber('1');
+    service.constructNumber('+');
+    service.constructNumber('2');
+    service.constructNumber('=');
+
+    expect(service.resultText()).toBe('3');
+  });
+
+  it('should calculate result correctly for subtraction "-"', () => {
+    service.constructNumber('5');
+    service.constructNumber('-');
+    service.constructNumber('2');
+    service.constructNumber('=');
+
+    expect(service.resultText()).toBe('3');
+  });
+
+  it('should calculate result correctly for multiplication "*"', () => {
+    service.constructNumber('2');
+    service.constructNumber('*');
+    service.constructNumber('3');
+    service.constructNumber('=');
+
+    expect(service.resultText()).toBe('6');
+  });
+
+  it('should calculate result correctly for division "/"', () => {
+    service.constructNumber('6');
+    service.constructNumber('/');
+    service.constructNumber('3');
+    service.constructNumber('=');
+
+    expect(service.resultText()).toBe('2');
+  });
+
+  it('shoudl handler decimal point correctly', () => {
+    service.constructNumber('1');
+    service.constructNumber('.');
+    service.constructNumber('5');
+    service.constructNumber('=');
+
+    expect(service.resultText()).toBe('1.5');
+    service.constructNumber('.');
+    expect(service.resultText()).toBe('1.5');
+  });
+
+  it('shoudl handler decimal point correctly starting with 0', () => {
+    service.constructNumber('0');
+    service.constructNumber('.');
+    service.constructNumber('0');
+
+    expect(service.resultText()).toBe('0.0');
+  });
+
 });
