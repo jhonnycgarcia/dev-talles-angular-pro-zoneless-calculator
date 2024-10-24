73d2a8a6aaab96932af177d6306fd8602df529cd
Jhonny <jhonnycgarcia@gmail.com>
2024-10-24 18:00:01 -0500

    CalculatorButton - HostElement

---
+++ src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CalculatorButtonComponent } from './calculator-button.component';
+
+describe('CalculatorButtonComponent', () => {
+
+  let fixture: ComponentFixture<CalculatorButtonComponent>;
+  let compiled: HTMLElement;
+  let component: CalculatorButtonComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CalculatorButtonComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CalculatorButtonComponent);
+    compiled = fixture.nativeElement  as HTMLElement;
+    fixture.detectChanges();
+    component = fixture.componentInstance;
+
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should apply w-1/4 class when isDoubleSize is false', () => {
+    const hostCssClasses: string[] = compiled.classList.value.split(' ');
+    expect(hostCssClasses).toContain('w-1/4');
+    expect(component.isDoubleSize()).toBeFalse();
+  });
+
+  it('should apply w-2/4 class when isDoubleSize is true', () => {
+    fixture.componentRef.setInput('isDoubleSize', true);
+    fixture.detectChanges();
+    const hostCssClasses: string[] = compiled.classList.value.split(' ');
+    expect(hostCssClasses).toContain('w-2/4');
+    expect(component.isDoubleSize()).toBeTrue();
+  });
+
+});
