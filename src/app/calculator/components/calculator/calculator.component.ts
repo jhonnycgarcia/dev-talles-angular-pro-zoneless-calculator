e41086c3cb30fe57a9380d486b39113d874e1b7d
Jhonny <jhonnycgarcia@gmail.com>
2024-10-23 22:54:48 -0500

    Separación de componentes

---
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculator.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CalculatorComponent {
+
+}
