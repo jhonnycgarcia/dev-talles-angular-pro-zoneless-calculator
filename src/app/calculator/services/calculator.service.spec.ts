3dce53563f51e2b9d4a17331e3f25524c1a61551
Jhonny <jhonnycgarcia@gmail.com>
2024-10-24 12:58:03 -0500

    Ciclo de vida - Reinicialización

--- src/app/calculator/services/calculator.service.spec.ts
+++ src/app/calculator/services/calculator.service.spec.ts
@@ -4,10 +4,21 @@ import { CalculatorService } from './calculator.service';
 describe('CalculatorService', () => {
   let service: CalculatorService;
 
+  // Antes de cada prueba
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(CalculatorService);
   });
+
+  // antes de todas las pruebas
+  beforeAll(() => {});
+
+  // despues de cada prueba
+  afterEach(() => {});
+
+  // despues de todas las pruebas
+  afterAll(() => {});
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
@@ -18,4 +29,17 @@ describe('CalculatorService', () => {
     expect(service.subResultText()).toBe('0');
     expect(service.lastOperator()).toBe('+');
   });
+
+  it('should set resultText to "0" when "C" is pressed', () => {
+    service.resultText.set('123');
+    service.subResultText.set('456');
+    service.lastOperator.set('*');
+
+    service.constructNumber('C');
+
+    expect(service.resultText()).toBe('0');
+    expect(service.subResultText()).toBe('0');
+    expect(service.lastOperator()).toBe('+');
+  });
+
 });
